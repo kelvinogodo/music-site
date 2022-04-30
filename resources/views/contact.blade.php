f9de16d8bf62a7e3301633050c235af4b51a142b
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2022-04-30 05:59:12 +0100

    edited gospel

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,23 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css folder/style.css">
+    <link rel="stylesheet" href="{{asset('data/css/style.css')}}">
     <title>contact me</title>
 </head>
 <body>
     <div class="blue-burble"></div>
     <header>
         <div class="logo-container">
-            <p>fashion vibes</p>
+            <p>FESHOW VIBES</p>
         </div>
         <div class="menu-list" id="menu-items">
             <div class="menu-list-login">
-                 <img src="../images/close.png" alt=""  id="close-sidebar" class="close-btn">
+                 <img src="{{asset('data/images/close.png')}}" alt=""  id="close-sidebar" class="close-btn">
             </div>
             <ul class="list-items">
-                <li class="li"><a href="../index.html">home </a ></li>
-                 <li class="li"><a href="./tracks.html">tracks</a></li>            
-                  <li class="li"><a href="./contact.html">contact</a></li>
+                <li class="li"><a href="{{route('index')}}">home </a ></li>
+                 <li class="li"><a href="{{route('tracks')}}">tracks</a></li>
+                  <li class="li"><a href="{{route(contact)}}">contact</a></li>
              </ul>
         </div>
         <!-- this is the code for the hamburger menu in the mobile view -->
@@ -31,9 +31,9 @@
         </div>
         <nav>
             <ul>
-                <li><a href="../index.html">home</a></li>
-                <li><a href="./tracks.html">tracks</a></li>
-                <li><a href="./contact.html" class="styled-anchor">contact</a></li>
+                <li><a href="{{route('index')}}">home</a></li>
+                <li><a href="{{route('tracks')}}">tracks</a></li>
+                <li><a href="{{route(contact)}}" class="styled-anchor">contact</a></li>
             </ul>
         </nav>
     </header>
@@ -42,11 +42,11 @@
             <h2>contact me</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus iure voluptatem nesciunt omnis?</p>
         </div>
-        
+
     </section>
     <section class="about-section contact-form-container">
         <div class="about-image-container">
-            <img src="../images/worship (2).jpg" alt="" class="profile-pics contact-img">
+            <img src="{{asset('data/images/worship (2).jpg')}}" alt="" class="profile-pics contact-img">
         </div>
         <div class="about-text-container ">
             <form action="" class="contact-form">
@@ -72,27 +72,27 @@
         <p class="header-3">Lorem ipsum dolor sit amet.</p>
         <section class="plans-section">
             <div class="plan-card">
-                <img src="../images/mail.png" alt="" class="icon">
+                <img src="{{asset('data/images/mail.png')}}" alt="" class="icon">
                 <p> johnDoe@gmail.com</p>
             </div>
             <div class="plan-card">
-                <img src="../images/phone.png" alt="" class="icon">
+                <img src="{{asset('data/images/phone.png')}}" alt="" class="icon">
                 <p>+234 7045783982 <br> +234 8046284682</p>
             </div>
             <div class="plan-card">
-                <img src="../images/down arrow.png" alt="" class="icon">
+                <img src="{{asset('data/images/down arrow.png')}}" alt="" class="icon">
                 <p>no:4 lagos island, LAGOS , NIGERIA</p></div>
             </div>
         </section>
         <footer>
             <div class="social-media-icon-container">
-                <a href=""><img src="../images/fb.png" alt="" class="social-media-icon"></a>
-                <a href=""><img src="../images/twitter.png" alt="" class="social-media-icon"></a>
-                <a href=""><img src="../images/insta.png" alt="" class="social-media-icon"></a>
-                <a href=""><img src="../images/whatsapp.png" alt="" class="social-media-icon"></a>
+                <a href=""><img src="{{asset('data/images/fb.png')}}" alt="" class="social-media-icon"></a>
+                <a href=""><img src="{{asset('data/images/twitter.png')}}" alt="" class="social-media-icon"></a>
+                <a href=""><img src="{{asset('data/images/insta.png')}}" alt="" class="social-media-icon"></a>
+                <a href=""><img src="{{asset('data/images/whatsapp.png')}}" alt="" class="social-media-icon"></a>
             </div>
             <p>&copy;copyright 2022 <br>All rights reserved</p>
         </footer>
-        <script src="../js folder/main.js"></script>
+        <script src="{{asset('data/js/main.js')}}"></script>
 </body>
 </html>
